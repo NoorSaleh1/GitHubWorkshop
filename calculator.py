fec1387c92abfbcea6a34c920e1ccf73fc6639c0
Noor Saleh <namerat45@gmail.com>
2024-07-13 13:47:24 +0300

    subtraction function

--- calculator.py
+++ calculator.py
@@ -1,6 +1,16 @@
+
 def addition(a, b):
 
     #Performs addition of two numbers.
 
     result = a + b
     return result
+
+def subtraction(a, b):
+
+    #Performs subtraction of two numbers.
+
+
+    result = a - b
+    return result
+

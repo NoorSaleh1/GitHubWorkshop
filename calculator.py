7208ea3dc1916efa98a78a598b2dd79c43e39a6e
Noor Saleh <namerat45@gmail.com>
2024-07-13 13:51:24 +0300

    division function

--- calculator.py
+++ calculator.py
@@ -21,3 +21,12 @@ def multiplication(a, b):
 
     result = a * b
     return result
+
+def division(a, b):
+
+    #Performs division of two numbers.
+
+    if b == 0:
+        return "Error: Cannot divide by zero."
+    result = a / b
+    return result
